a9741abb2b82132fb5130b7c53cd80e96a9c8900
stayrahul <rahul7926963@gmail.com>
2025-05-21 23:34:27 +0545

    Add Google AdSense scripts to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";  // import Next.js Script component
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import type { PropsWithChildren } from "react";
@@ -20,9 +21,34 @@ export const metadata: Metadata = siteConfig;
 const RootLayout = ({ children }: Readonly<PropsWithChildren>) => {
   return (
     <html lang="en">
+      <head>
+        {/* Add the first async AdSense script */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7260033887122512"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark">
           {children}
+          {/* Add the adsbygoogle script to initialize ads */}
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-7260033887122512"
+            data-ad-slot="3088041180"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <Script
+            id="adsbygoogle-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(adsbygoogle = window.adsbygoogle || []).push({});`,
+            }}
+          />
         </ThemeProvider>
       </body>
     </html>
